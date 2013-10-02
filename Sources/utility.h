9db92ed1d96f194db5c8f1a2b21c49c096661e22
NeverSeen <neverseenone@gmail.com>
2013-10-02 16:49:19 +0300

    Attribute file added.

--- Sources/utility.h
+++ Sources/utility.h
@@ -1,7 +1,12 @@
+#pragma once
+#include "mapAttributes.h"
 
-clan::vec2 pixel_to_cunk_pos (const clan::vec2 pixel_pos, const clan::vec2 offset)
+clan::vec2 pixel_to_cunk_pos (const clan::vec2 pixel_pos)
 {
-	clan::vec2 ats=pixel_pos;
+	clan::vec2 ats;
+
+	ats.x = (pixel_pos.x % CHUNK_EDGE_LENGTH_PIXELS)*CHUNK_EDGE_LENGTH_PIXELS;
+	ats.y = (pixel_pos.y % CHUNK_EDGE_LENGTH_PIXELS)*CHUNK_EDGE_LENGTH_PIXELS;
 
 	return ats;
 }
