8a6edada4c49bc087e26ca9cf6f972476e0df821
NeverSeen <neverseenone@gmail.com>
2014-01-12 19:55:06 +0200

    Signed-off-by: NeverSeen <neverseenone@gmail.com>

--- Sources/game_objects/player.cpp
+++ Sources/game_objects/player.cpp
@@ -147,7 +147,8 @@ void Player::render(clan::Canvas & c, const clan::vec2 & offset)
 	if(!m_sprite.is_null())
 	{
 		m_sprite.draw(c, m_pos.get().x+offset.x, m_pos.get().y+offset.y);
-		c.fill_rect(m_pos.get().x+offset.x+10, m_pos.get().y+offset.y-1,  m_pos.get().x+offset.x+10+(44*life/100), m_pos.get().y+offset.y+8,  get_hp_bar_color());
+		c.fill_rect(m_pos.get().x+offset.x+10, m_pos.get().y+offset.y-1,  m_pos.get().x+offset.x+10+(44*life/100), m_pos.get().y+offset.y+4,  get_hp_bar_color());
+		c.fill_rect(m_pos.get().x+offset.x+10, m_pos.get().y+offset.y+4,  m_pos.get().x+offset.x+10+(44*mana/100), m_pos.get().y+offset.y+8,  clan::Colorf(0.5f,0.5f,1.0f,1.0f));
 		m_h.draw(c,m_pos.get().x+offset.x, m_pos.get().y+offset.y-5);
 		m_outline.draw(offset.x,offset.y,clan::Colorf(1,0,0,1),c);
 
